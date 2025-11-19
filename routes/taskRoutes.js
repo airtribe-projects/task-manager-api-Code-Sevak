b2005ed05c7a3da798d139f557332a4122ca423b
code-sevak <shrishanth@SHRISHANTHs-MacBook-Air.local>
2025-11-19 23:11:48 +0530

    updated

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const tasks = require('../models/taskSchema');
-
+const tasksList = require('../task.json');
+const tasks = tasksList.tasks
 router.get('/', (req, res) => {
   res.status(200).send(tasks);
 });
